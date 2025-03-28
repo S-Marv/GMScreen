afb015f1e60ac564f19e4bc08f80ca1f77ce7a8a
Samuel Marvin <97563317+S-Marv@users.noreply.github.com>
2025-03-28 01:41:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
+    base: '/<REPO>/',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
