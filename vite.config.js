3acdb1057dd25a44f14bb55a76f6a210c3acc76c
Samuel Marvin <97563317+S-Marv@users.noreply.github.com>
2025-03-28 01:37:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://S-Marv.github.io/GMScreen",
+  base: '/<REPO>/',
   plugins: [
     vue(),
     vueDevTools(),
