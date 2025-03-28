49947645151a78ac3968a1485cb17b3cbda81e51
Samuel Marvin <97563317+S-Marv@users.noreply.github.com>
2025-03-28 01:44:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/GMScreen/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
-    base: '/GMScreen/',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
